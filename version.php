96319e71f727d59d974170862bf47658d8cabf78
Umme Kawser Sinthia <sinthy08@gmail.com>
2021-12-30 13:16:45 +0600

    version update

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'enrol_bkash';    // Full name of the plugin (used for diagnostics).
-$plugin->version = 2021122300;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2021123000;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->release = 'v1.0.1';
 $plugin->requires = 2020061509;        // Requires this Moodle version.
 $plugin->maturity = MATURITY_STABLE;
